ad77b750930176157fd28b2a5eba34bc0d49f01c
Desmond Bowe <desmondbowe@gmail.com>
2012-09-18 22:03:07 -0400

    2nd pass: object-oriented

--- wip.rb
+++ wip.rb
@@ -1,11 +1,80 @@
 #!/usr/bin/ruby
 
-def local_branches
-  `git branch --list`.gsub(/[ |\*]/,'').split("\n")
-end
+class Wip
 
-def remote_branches
-  `git branch --remote`.gsub('origin/','').gsub(/ -\>.*/,'').split("\n")
+  def self.process(argv)
+    validate_input(argv)
+
+    wip = new(argv.first)
+    wip.validate_branches
+    wip.validate_origin_exists
+    wip.create_branch_and_push
+  end
+
+  def initialize(wip_branch)
+    @wip_branch = wip_branch
+  end
+
+  def validate_origin_exists
+    if `git remote` == ''
+      puts "no origin configured"
+      exit
+    end
+  end
+
+  def local_branches
+    `git branch --list`.gsub(/[ |\*]/,'').split("\n")
+  end
+
+  def remote_branches
+    `git branch --remote`.gsub('origin/','').gsub(/ -\>.*/,'').split("\n")
+  end
+
+  def branch_exists_locally?
+    local_branches.any? { |local| local == @wip_branch }
+  end
+
+  def branch_exists_remotely?
+    remote_branches.any? { |remote| remote == @wip_branch }
+  end
+
+
+  def validate_branches
+    if branch_exists_locally? @wip_branch
+      puts "Local branch #{@wip_branch} exists!"
+      exit
+    elsif branch_exists_remotely? @wip_branch
+      puts "Remote branch #{@wip_branch} exists!"
+      exit
+    end
+  end
+
+  def create_branch_and_push
+    `git checkout -b #{@wip_branch}`
+    `git add -u . `
+    `git commit -m "WIP"`
+    `git push origin #{@wip_branch}` # assumes remote is at origin
+  end
+
+  def self.validate_input(argv)
+    if argv.count == 0
+      puts "Please enter a branch name!"
+      exit
+    elsif argv.count > 1
+      puts "Name can't have spaces!"
+      exit
+    end
+
+    if argv.first == 'HEAD'
+      puts "Branch name can't be HEAD"
+      exit
+    end
+
+    if argv.first == 'master'
+      puts "I'm not pushing to master"
+      exit
+    end
+  end
 end
 
 def all_branches
@@ -18,81 +87,5 @@ def all_branches
     # remotes/origin/master
 end
 
-def branch_exists_locally?(branch)
-  local_branches.any? { |local| local == branch }
-end
-
-def branch_exists_remotely?(branch)
-  remote_branches.any? { |remote| remote == branch }
-end
-
-def validate_inputs
-  if ARGV.count == 0
-    puts "Please enter a branch name!"
-    exit
-  elsif ARGV.count > 1
-    puts "Name can't have spaces!"
-    exit
-  end
-
-  if ARGV.first == 'HEAD'
-    puts "Branch name can't be HEAD"
-    exit
-  end
-
-  if ARGV.first == 'master'
-    puts "I'm not pushing to master"
-    exit
-  end
-end
-
-def validate_origin_exists
-  if `git remote` == ''
-    puts "no origin configured"
-    exit
-  end
-end
-
-def validate_branches(branch)
-  if branch_exists_locally? branch
-    puts "Local branch #{branch} exists!"
-    exit
-  elsif branch_exists_remotely? branch
-    puts "Remote branch #{branch} exists!"
-    exit
-  end
-end
-
-def create_local_branch(branch)
-  puts "creating local branch"
-  `git checkout -b #{branch}`
-end
-
-def stage_all_files
-  puts "staging"
-  `git add -u . `
-end
-
-def commit_wip
-  puts "committing"
-  `git commit -m "WIP"`
-end
-
-def push_wip(branch)
-  # assumes remote is at origin
-  puts "pushing to remote"
-  `git push origin #{branch}`
-end
-
-
-validate_inputs
-branch = ARGV.first
-validate_origin_exists
-validate_branches(branch)
-
-create_local_branch(branch)
-stage_all_files
-commit_wip
-push_wip(branch)
-
+Wip.process(ARGV)
 
